c43901d29953e543579cdd6370fcbeedc8748afe
Jonathan Williams <tamlin@mac.com>
2023-04-18 11:50:18 -0400

    use a simple php and apache image
    instead of the default node image

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Sample Page</title>
+</head>
+<body>
+    <div>Hello world</div>
+    <div>Name from container environment is: <?php echo $_ENV['name'] ; ?></div>
+</body>
+</html>
